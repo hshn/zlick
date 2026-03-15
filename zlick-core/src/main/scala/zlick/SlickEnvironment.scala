da9a3d0cfcedf8d719ace572a179984643c83ede
Shota Hoshino <sht.hshn@gmail.com>
2026-03-15 15:39:26 +0900

    feat: return ZLayer from SlickEnvironment and support forURI

--- zlick-core/src/main/scala/zlick/SlickEnvironment.scala
+++ zlick-core/src/main/scala/zlick/SlickEnvironment.scala
@@ -2,23 +2,33 @@ package zlick
 
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
+import java.net.URI
 import scala.reflect.ClassTag
+import slick.basic.BasicProfile
 import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
 import zio.Scope
+import zio.Tag
 import zio.ZIO
+import zio.ZLayer
 
 object SlickEnvironment {
 
   /** Creates a scoped [[DatabaseConfig]] that automatically closes the database when the scope ends. */
-  def config[P <: JdbcProfile: ClassTag](config: Config, path: String): ZIO[Scope, Throwable, DatabaseConfig[P]] =
+  def forConfig[P <: BasicProfile: ClassTag](config: Config, path: String)(using Tag[P]): ZLayer[Scope, Throwable, DatabaseConfig[P]] =
+    configure(DatabaseConfig.forConfig[P](path = path, config = config))
+
+  /** Creates a scoped [[DatabaseConfig]] that automatically closes the database when the scope ends. */
+  def forConfig[P <: BasicProfile: ClassTag](path: String)(using Tag[P]): ZLayer[Scope, Throwable, DatabaseConfig[P]] =
+    forConfig[P](path = path, config = ConfigFactory.load())
+
+  def forURI[P <: BasicProfile: ClassTag](uri: URI)(using Tag[P]): ZLayer[Scope, Throwable, DatabaseConfig[P]] =
+    configure(DatabaseConfig.forURI[P](uri))
+
+  private def configure[P <: BasicProfile](f: => DatabaseConfig[P])(using Tag[P]) = ZLayer.scoped {
     ZIO
-      .attempt(DatabaseConfig.forConfig[P](path = path, config = config))
+      .attempt(f)
       .withFinalizer { databaseConfig =>
         ZIO.attemptBlocking(databaseConfig.db.close()).orDie
       }
-
-  /** Creates a scoped [[DatabaseConfig]] that automatically closes the database when the scope ends. */
-  def config[P <: JdbcProfile: ClassTag](path: String): ZIO[Scope, Throwable, DatabaseConfig[P]] =
-    config[P](path = path, config = ConfigFactory.load())
+  }
 }
